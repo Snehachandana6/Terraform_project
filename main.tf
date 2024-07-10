abe8ae3edd7309b14274c8ae250c4a389b9bb425
Siri Candana Maheshwaram <sirimaheshwaram2001@gmail.com>
2024-07-10 16:25:03 +0530

    latest

--- main.tf
+++ main.tf
@@ -6,5 +6,8 @@ module "ec2_instance" {
   source = "./module/ec2_instance"
   ami_value = "ami-0a0e5d9c7aacc336f1"
   instance_type_value = "t3.micro"
-  tag_value = "ec2_machine_first"
+  tag_value = {
+    name = "ec2_machine_first"
+  }
+
 }

7b32aa65fc561157b4e23280427a23b526937d8e
Siri Candana Maheshwaram <sirimaheshwaram2001@gmail.com>
2024-07-10 16:33:03 +0530

    latestcode

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ module "ec2_instance" {
   source = "./module/ec2_instance"
   ami_value = "ami-0a0e5d9c7aacc336f1"
   instance_type_value = "t3.micro"
+
   tag_value = {
     name = "ec2_machine_first"
   }
-
 }
